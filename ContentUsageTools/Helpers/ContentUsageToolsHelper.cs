01a436b0023d6b60277febdbc94cf9185e808823
Johann Baziret <Johann@sidewalk.be>
2014-01-25 04:17:17 +0100

    Add ContentUsageToolsHelper

---
+++ ContentUsageTools/Helpers/ContentUsageToolsHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Data.Items;
+
+namespace ContentUsageTools.Helpers
+{
+    public class ContentUsageToolsHelper
+    {
+        /// <summary>
+        /// Return a list of item which is linked to the item in parameters
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public IEnumerable<Item> LinkedItems(Item item)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Check if an item has a presentation
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public bool HasPresentation(Item item)
+        {
+            // TODO: Call the pipeline HasPresentationSettings
+            return true;
+        }
+
+        /// <summary>
+        /// Check if an item is UnUsed 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public bool IsUnused(Item item)
+        {
+            
+        }
+
+
+    }
+}
