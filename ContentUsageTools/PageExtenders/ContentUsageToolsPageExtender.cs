1c67b8b03e48db2a8948aa7dfc1d0bbbeefa3775
Robin Hermanussen <r.hermanussen@lukkien.com>
2014-01-25 07:08:12 +0100

    Added a way to extend the page editor html (in this case, inject a JS file)

---
+++ ContentUsageTools/PageExtenders/ContentUsageToolsPageExtender.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Sitecore.Layouts;
+using Sitecore.Layouts.PageExtenders;
+using Sitecore.Shell.Applications.ContentEditor;
+using Sitecore.Sites;
+using Sitecore.Web;
+using Sitecore.Web.UI.HtmlControls;
+using Literal = Sitecore.Web.UI.HtmlControls.Literal;
+
+namespace ContentUsageTools.PageExtenders
+{
+    /// <summary>
+    /// Inserts page editor contributions to the page.
+    /// </summary>
+    public class ContentUsageToolsPageExtender : PageExtender
+    {
+        public override void Insert()
+        {
+            SiteContext site = Sitecore.Context.Site;
+            if ((site != null)
+                && site.EnableWebEdit
+                && site.DisplayMode == DisplayMode.Edit
+                && WebUtil.GetQueryString("sc_webedit") != "0")
+            {
+                RenderingReference reference = new RenderingReference(
+                    new Literal()
+                        {
+                            Text = "<script type=\"text/javascript\" src=\"/sitecore modules/Content Usage Tools/js/ContentUsageTools.js\"></script>"
+                        });
+                reference.Placeholder = "webedit";
+                reference.AddToFormIfUnused = true;
+                Sitecore.Context.Page.AddRendering(reference);
+            }
+        }
+    }
+}
