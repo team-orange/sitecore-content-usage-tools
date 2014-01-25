7f6eabe598d6826e39a1669d743ba8bb634ef503
Robin Hermanussen <r.hermanussen@lukkien.com>
2014-01-25 12:17:03 +0100

    Exclude the current page when determining references

--- ContentUsageTools/Commands/ShowComponentReferencesCommand.cs
+++ ContentUsageTools/Commands/ShowComponentReferencesCommand.cs
@@ -1,4 +1,7 @@
-﻿using ContentUsageTools.Helpers;
+﻿using System.Web;
+using ContentUsageTools.Helpers;
+using Sitecore.Collections;
+using Sitecore.Data;
 using Sitecore.Data.Items;
 using Sitecore.Diagnostics;
 using Sitecore.Links;
@@ -19,21 +22,46 @@ namespace ContentUsageTools.Commands
         public override void Execute(CommandContext context)
         {
             Assert.ArgumentNotNull(context, "context");
-
             if (context == null)
+            {
                 return;
+            }
 
             Item current = context.Items.FirstOrDefault();
 
             if(current != null)
             {
-                this.RetrieveReferences(current);
+                RetrieveReferences(current);
             }
+        }
+
+        private static Item GetPageItem()
+        {
+            if (Sitecore.Context.Database != null 
+                && HttpContext.Current != null
+                && HttpContext.Current.Request.UrlReferrer != null)
+            {
+                SafeDictionary<string> qsParams = WebUtil.ParseQueryString(HttpContext.Current.Request.UrlReferrer.Query);
+                if (qsParams != null && qsParams.ContainsKey("id") && qsParams.ContainsKey("db"))
+                {
+                    Database database = Database.GetDatabase(qsParams["db"]);
+                    return database != null
+                        ? database.GetItem(HttpUtility.UrlDecode(qsParams["id"]))
+                        : null;
+                }
+            }
+            return null;
         }
 
         private void RetrieveReferences(Item current)
         {
+            Item pageItem = GetPageItem();
+
             List<Item> references = ContentUsageToolsHelper.GetLinkedItems(current).ToList();
+            if (pageItem != null)
+            {
+                references = references.Where(r => r.ID != pageItem.ID).ToList();
+            }
 
             if(references != null && references.Any())
             {
