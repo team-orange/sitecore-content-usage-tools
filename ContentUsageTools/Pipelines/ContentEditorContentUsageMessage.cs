7f68f887037002633e53fd1b12085112de6c3a6d
Robin Hermanussen <r.hermanussen@lukkien.com>
2014-01-25 05:34:33 +0100

    Content editor message implemented

--- ContentUsageTools/Pipelines/ContentEditorContentUsageMessage.cs
+++ ContentUsageTools/Pipelines/ContentEditorContentUsageMessage.cs
@@ -12,20 +12,39 @@ using Sitecore.Web.UI.Sheer;
 
 namespace ContentUsageTools.Pipelines
 {
+    /// <summary>
+    /// Shows a message in the content editor to display on which pages the item is being used as a datasource.
+    /// </summary>
     public class ContentEditorContentUsageMessage
     {
         public void Process(GetContentEditorWarningsArgs args)
         {
             if (args.Item == null
-                || ContentUsageToolsHelper.IsPage(args.Item)) // don't show this message for pages themselves
+                || ContentUsageToolsHelper.IsPage(args.Item) // don't show this message for pages themselves
+                || args.Item.Fields.All(f => f.Name.StartsWith("_"))) 
             {
                 return;
             }
-            
-            GetContentEditorWarningsArgs.ContentEditorWarning
-			contentEditorWarning = args.Add();
-            contentEditorWarning.Title = Translate.Text("TODO: display stuff here.");
-            contentEditorWarning.AddOption(Translate.Text("TODO: Perhaps navigate"), "contenteditorusage:navigate");
+
+            if (ContentUsageToolsHelper.IsUnused(args.Item))
+            {
+                // Display a message suggesting to remove this item
+                GetContentEditorWarningsArgs.ContentEditorWarning contentEditorWarning = args.Add();
+                contentEditorWarning.Title = Translate.Text("This item is not being used on any page.");
+                contentEditorWarning.AddOption(Translate.Text("Delete this item"), "item:delete");
+            }
+            else
+            {
+                // Display what pages the item is being used on as a datasource, so you can easily navigate to them
+                GetContentEditorWarningsArgs.ContentEditorWarning contentEditorWarning = args.Add();
+                contentEditorWarning.Title = Translate.Text("This item is being used on the following pages:");
+                foreach (Item linkedPage in ContentUsageToolsHelper.GetLinkedItems(args.Item))
+                {
+                    string text = string.Format("Open {0} <i>({1})</i>", linkedPage.DisplayName, linkedPage.Paths.ContentPath);
+                    string command = string.Format("item:load(id={0})", linkedPage.ID);
+                    contentEditorWarning.AddOption(Translate.Text(text), command);
+                }
+            }
         }
     }
 }
