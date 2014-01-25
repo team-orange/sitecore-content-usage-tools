dbbe97068b1f178e9a037f25e52866f296ebbd71
Dražen Janjiček <drazen.janjicek@hinttech.com>
2014-01-25 19:41:03 +0100

    Added class for comparer

---
+++ ContentUsageTools/Helpers/ItemEqualityComparer.cs
@@ -0,0 +1,51 @@
+﻿/* Copyright (C) 2014 Dražen Janjiček, Johann Baziret, Robin Hermanussen
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see http://www.gnu.org/licenses/. */
+
+namespace ContentUsageTools.Helpers
+{
+    using Sitecore.Data.Items;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Compares two Sitecore items.
+    /// </summary>
+    public class ItemEqualityComparer : IEqualityComparer<Item>
+    {
+        /// <summary>
+        /// Determines whether the specified objects are equal.
+        /// </summary>
+        /// <param name="x">The first object of type <paramref name="T" /> to compare.</param>
+        /// <param name="y">The second object of type <paramref name="T" /> to compare.</param>
+        /// <returns>
+        /// true if the specified objects are equal; otherwise, false.
+        /// </returns>
+        public bool Equals(Item x, Item y)
+        {
+            return x.ID.Equals(y.ID);
+        }
+
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table. 
+        /// </returns>
+        public int GetHashCode(Item obj)
+        {
+            return obj.ID.GetHashCode();
+        }
+    }
+}
